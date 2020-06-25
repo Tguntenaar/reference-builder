42f87b95775204e5ab6a0a5403b0c8b486e0696d
Thomas Guntenaar <thomas@guntenaar.org>
2020-06-26 01:26:51 +0200

    folder settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,35 +18,3 @@ module.exports = {
   },
   plugins: ['prettier'],
 };
-// {
-//   env: {
-//     browser: true,
-//     es2020: true,
-//   },
-//   extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
-//   parserOptions: {
-//     ecmaFeatures: {
-//       jsx: true,
-//     },
-//     ecmaVersion: 11,
-//     sourceType: 'module',
-//   },
-//   plugins: ['react', 'prettier'],
-//   rules: {
-//     'react/jsx-filename-extension': [
-//       1,
-//       {
-//         extensions: ['.js', '.jsx'],
-//       },
-//     ],
-//     'react/jsx-props-no-spreading': 'off',
-//     'prettier/prettier': [
-//       'error',
-//       {
-//         trailingComma: 'es5',
-//         singleQuote: true,
-//         printWidth: 100,
-//       },
-//     ],
-//   },
-// };
